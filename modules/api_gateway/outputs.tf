e24af2fd3218794c185fd1c1267c6a70bb89eef8
AGUTI39 <aguti39@bancodebogota.com.co>
2025-03-20 20:30:02 -0500

    ajuste

--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -5,5 +5,5 @@ output "api_gateway_id" {
 
 output "api_gateway_invoke_url" {
   description = "URL base del API Gateway"
-  value       = length(aws_api_gateway_stage.stage) > 0 ? aws_api_gateway_stage.stage[0].invoke_url : null
+  value       = aws_api_gateway_stage.stage.invoke_url
 }
